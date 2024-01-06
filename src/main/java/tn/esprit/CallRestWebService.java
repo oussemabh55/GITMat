a8c4d4f1b240430cf57141971e6fb61a36ff9324
oussema Ben hmaid <oussemabenhmaid98@gmail.com>
2024-01-06 16:30:41 +0100

    dernier

--- src/main/java/tn/esprit/CallRestWebService.java
+++ src/main/java/tn/esprit/CallRestWebService.java
@@ -21,7 +21,7 @@ import org.json.JSONObject;
 
 public class CallRestWebService {
 	//helloworld
-	//test oussama
+	//testmostspha
 	public static final String endpoint = "http://ip-api.com/json";
 	//public static final String endpoint = "https://httpbin.org/get";
 	public static void main(String[] args) {
